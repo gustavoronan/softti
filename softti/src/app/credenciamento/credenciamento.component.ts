363ccad631f71ed3b905b8ccb75f5769bbe4ac9c
Gustavo Ronan <gustavoronn@gmail.com>
2025-01-09 23:16:44 -0300

    add: finalizando a modal de cadastro

--- softti/src/app/credenciamento/credenciamento.component.ts
+++ softti/src/app/credenciamento/credenciamento.component.ts
@@ -1,13 +1,16 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-credenciamento',
   templateUrl: './credenciamento.component.html',
   styleUrls: ['./credenciamento.component.css'],
+  imports: [FormsModule],
   standalone: true 
 })
 export class CredenciamentoComponent {
   @ViewChild('myModal') model!: ElementRef;
+  clientObj: Cliente  = new Cliente()
 
   openModel() {
     if (this.model) {
@@ -19,5 +22,31 @@ export class CredenciamentoComponent {
     if (this.model) {
       this.model.nativeElement.style.display = 'none';
     }
+  }
+}
+
+export class Cliente {
+  tipo!: 'fisica' | 'juridica';
+  documento!: string;
+  nomeRazaoSocial!: string;
+  nomeFantasia!: string;
+  cep!: string;
+  endereco!: string;
+  bairro!: string;
+  cidade!: string;
+  telefone!: string;
+  email!: string;
+
+  constructor(){
+    this.documento = '';
+    this.documento = "";
+    this.nomeRazaoSocial = '';
+    this.nomeFantasia = '';
+    this.cep = '';
+    this.endereco = '';
+    this.bairro = '';
+    this.cidade = '';
+    this.telefone = '';
+    this.email = '';
   }
 }
